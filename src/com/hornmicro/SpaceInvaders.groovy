ade81bc9b70c40d90ecff5239ef36f838c548208
Scott Horn <scott@hornmicro.com>
2012-04-17 17:11:00 +1000

    Added shooting and some vader sprites

--- src/com/hornmicro/SpaceInvaders.groovy
+++ src/com/hornmicro/SpaceInvaders.groovy
@@ -15,7 +15,8 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener
 import org.eclipse.swt.widgets.Shell
 import org.pushingpixels.trident.Timeline
-import org.pushingpixels.trident.ease.Linear;
+import org.pushingpixels.trident.Timeline.TimelineState
+import org.pushingpixels.trident.callback.TimelineCallback
 import org.pushingpixels.trident.swt.SWTRepaintTimeline;
 
 class SpaceInvaders implements PaintListener, DisposeListener, Listener {
@@ -24,11 +25,10 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
     Timeline timeline
     SWTRepaintTimeline paintTimeline
     int left = 1
+    Point bullet = new Point(0,-20)
     
     void setLeft(int left) {
         this.left = left
-        println left
-//        paintTimeline.forceRepaintOnNextPulse()
         shell.display.asyncExec { shell.redraw() }
     }
     
@@ -37,13 +37,18 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
         gc.setAdvanced(true)
         gc.setAntialias(SWT.ON)
         
-        gc.setAlpha(200)
-        gc.setBackground(shell.display.getSystemColor(SWT.COLOR_BLACK))
-        gc.fillRectangle(shell.getClientArea())
+//        gc.setAlpha(200)
+//        gc.setBackground(shell.display.getSystemColor(SWT.COLOR_BLACK))
+//        gc.fillRectangle(shell.getClientArea())
         
         if(!spacecraft) {
             spacecraft = new Image(Display.current, "spacecraft.png")
         }
+        if(bullet.y != -20) {
+            gc.setBackground(shell.display.getSystemColor(SWT.COLOR_GRAY))
+            gc.fillRectangle(bullet.x, bullet.y-20, 10, 20)
+        }
+         
         gc.setAlpha(255)
         gc.drawImage(spacecraft, 
             0, 0, spacecraft.width, spacecraft.height, 
@@ -59,6 +64,7 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
         if(event.type == SWT.KeyDown && lastkey != event.keyCode) {
             Rectangle screen = shell.display.getBounds()
             int newLoc
+            Point newBullet 
             switch(event.keyCode) {
                case 16777220:
                    newLoc = screen.width - spacecraft.width*2
@@ -66,12 +72,31 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
                case 16777219:
                    newLoc = 1
                    break
+               case 32:
+                   if(bullet.y == -20) {
+                       newBullet = new Point(left + spacecraft.width - 5, screen.height - spacecraft.height*2)
+                       Timeline bulletTimeLine = new Timeline(this)
+                       bulletTimeLine.addPropertyToInterpolate("bullet", newBullet, new Point(newBullet.x, -20))
+                       bulletTimeLine.setDuration(screen.height)
+                       bulletTimeLine.addCallback(new TimelineCallback() {
+                           void onTimelinePulse(float arg0, float arg1) {
+                               shell.display.asyncExec { shell.redraw() }
+                           }
+                           void onTimelineStateChanged(TimelineState arg0, TimelineState arg1, float arg2, float arg3) {
+                           }
+                       })
+                       bulletTimeLine.play()
+                   }
+                   return
+                   break;
                default:
+                   println event.keyCode
                    return
             }
             timeline?.cancel()
             timeline = new Timeline(this)
-            timeline.setDuration(Math.abs(newLoc - left))
+            
+            timeline.setDuration( (Math.abs(newLoc - left) / 2) as int)
             timeline.addPropertyToInterpolate("left", left, newLoc)
             timeline.play()
             
@@ -91,7 +116,7 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
         display.addFilter(SWT.KeyUp, this)
         Rectangle screen = display.getBounds()
         
-        shell = new Shell(display, SWT.NO_TRIM | SWT.NO_BACKGROUND | SWT.ON_TOP)
+        shell = new Shell(display, SWT.NO_TRIM | SWT.NO_BACKGROUND | SWT.ON_TOP | SWT.DOUBLE_BUFFERED)
         //shell.setBounds(100, screen.height - 60, 56, 40)
         shell.setBounds(1,25, screen.width, screen.height-25)
         shell.addPaintListener(this)
