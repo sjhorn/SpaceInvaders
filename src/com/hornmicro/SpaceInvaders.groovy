d7d376672752949a981794a257e0fb225e4195fb
Scott Horn <scott@hornmicro.com>
2012-04-18 10:11:50 +1000

    Added animate vader

--- src/com/hornmicro/SpaceInvaders.groovy
+++ src/com/hornmicro/SpaceInvaders.groovy
@@ -15,17 +15,23 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener
 import org.eclipse.swt.widgets.Shell
 import org.pushingpixels.trident.Timeline
+import org.pushingpixels.trident.Timeline.RepeatBehavior;
 import org.pushingpixels.trident.Timeline.TimelineState
 import org.pushingpixels.trident.callback.TimelineCallback
-import org.pushingpixels.trident.swt.SWTRepaintTimeline;
+import org.pushingpixels.trident.callback.TimelineCallbackAdapter
+import org.pushingpixels.trident.interpolator.KeyFrames
+import org.pushingpixels.trident.interpolator.KeyTimes
+import org.pushingpixels.trident.interpolator.KeyValues
 
 class SpaceInvaders implements PaintListener, DisposeListener, Listener {
-    Image spacecraft
+    Image spacecraft, vader1Left, vader1Right
     Shell shell
-    Timeline timeline
-    SWTRepaintTimeline paintTimeline
-    int left = 1
-    Point bullet = new Point(0,-20)
+    Timeline timeline, vaderTimeline
+    int vaderToggle = 0
+    
+    
+    int left = 1, vaderLeft = 0
+    Point bullet = new Point(0, -20)
     
     void setLeft(int left) {
         this.left = left
@@ -42,7 +48,9 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
 //        gc.fillRectangle(shell.getClientArea())
         
         if(!spacecraft) {
-            spacecraft = new Image(Display.current, "spacecraft.png")
+            spacecraft = new Image(shell.display, "spacecraft.png")
+            vader1Left = new Image(shell.display, "vader1_left.png")
+            vader1Right = new Image(shell.display, "vader1_right.png")
         }
         if(bullet.y != -20) {
             gc.setBackground(shell.display.getSystemColor(SWT.COLOR_GRAY))
@@ -50,6 +58,11 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
         }
          
         gc.setAlpha(255)
+        gc.drawImage(vaderToggle ? vader1Left : vader1Right,
+            0, 0, vader1Left.width, vader1Left.height,
+            10, 10, vader1Left.width * 2, vader1Left.height * 2)
+        
+        
         gc.drawImage(spacecraft, 
             0, 0, spacecraft.width, spacecraft.height, 
             left, shell.display.bounds.height - 60, spacecraft.width * 2, spacecraft.height * 2)
@@ -67,7 +80,7 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
             Point newBullet 
             switch(event.keyCode) {
                case 16777220:
-                   newLoc = screen.width - spacecraft.width*2
+                   newLoc = screen.width - spacecraft.width * 2
                    break
                case 16777219:
                    newLoc = 1
@@ -117,14 +130,26 @@ class SpaceInvaders implements PaintListener, DisposeListener, Listener {
         Rectangle screen = display.getBounds()
         
         shell = new Shell(display, SWT.NO_TRIM | SWT.NO_BACKGROUND | SWT.ON_TOP | SWT.DOUBLE_BUFFERED)
-        //shell.setBounds(100, screen.height - 60, 56, 40)
         shell.setBounds(1,25, screen.width, screen.height-25)
         shell.addPaintListener(this)
         shell.addDisposeListener(this)
         shell.window.setOpaque(false);
         shell.window.setBackgroundColor(NSColor.clearColor())
-//        paintTimeline = new SWTRepaintTimeline(shell)
-//        paintTimeline.setAutoRepaintMode(false);
+
+        KeyValues vaderValues = KeyValues.create(1, 1, 0, 0)
+        KeyTimes vaderTimes = new KeyTimes(0.0f, 0.49f, 0.5f,  1.0f)        
+        vaderTimeline = new Timeline(this)
+        vaderTimeline.addPropertyToInterpolate("vaderToggle", new KeyFrames(vaderValues, vaderTimes))
+        vaderTimeline.setDuration(1000)
+        vaderTimeline.addCallback(new TimelineCallbackAdapter() {
+            @Override
+            public void onTimelinePulse(float durationFraction,
+                    float timelinePosition) {
+                shell.display.asyncExec { shell.redraw() }
+            }
+        })
+        vaderTimeline.playLoop(RepeatBehavior.REVERSE)
+        
         shell.open()
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch())
